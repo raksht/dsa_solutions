37e86524067d6c9f73f6a17a4f2853e338673f9f
Rakshitha T <166599397+raksht@users.noreply.github.com>
2026-07-23 19:05:11 +0530

    Create Pattern14.cpp

---
+++ Patterns/Pattern14.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+class Solution {
+public:
+    void pattern14(int n) {
+        for(int i=0;i<=n;i++){
+            for(char ch='A';ch<='A'+i;ch++){
+                cout<<ch<<" "
+            }
+            cout<<endl;
+        }
+    }
+};
+int main(){
+    Solution obj;
+    int i;
+    cout<<"Enter the number of rows:";
+    cin>>i;
+    obj.pattern14(i);
+    return 0;
+}
